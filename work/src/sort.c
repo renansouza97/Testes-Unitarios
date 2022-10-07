720a3a2c05eb31e87e92079afea0559008bc1c61
renansouza97 <renansouzasilva01@gmail.com>
2022-10-07 18:31:31 -0300

    Teste 2

--- work/src/sort.c
+++ work/src/sort.c
@@ -4,7 +4,7 @@ int sort(int* a, int length, char* type, int algorithm) {
     int status = 0;
     int *s = (int*)malloc(sizeof(int)*length);
     memcpy(s,a,sizeof(int)*(length));
-    if (length < 2 || length > 20) {
+    if (length <= 2 || length > 20) {
         status = 1;
     }
     else {
@@ -13,7 +13,7 @@ int sort(int* a, int length, char* type, int algorithm) {
                 if (!strcmp(type,"On"))
                     counting_sort(s, length);
                 else
-                    status = 0;
+                    status = 1;
                 break;
             case RADIX:
                 if (!strcmp(type,"On"))
